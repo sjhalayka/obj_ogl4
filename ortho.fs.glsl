f8a66e535507c836c63799ca06664eecc36347a3
Shawn Halayka <sjhalayka@gmail.com>
2022-10-22 14:04:27 -0600

    Add files via upload

--- ortho.fs.glsl
+++ ortho.fs.glsl
@@ -38,35 +38,27 @@ void main()
     float directions = 16.0; // BLUR directions (Default 16.0 - More is better but slower)
     float quality = 10.0; // BLUR quality (Default 4.0 - More is better but slower)
     float size = 8.0; // BLUR size (radius)
-   
+
+	vec4 unblurred_colour = texture(colour_tex, ftexcoord);
+	float depth_colour = texture(depth_tex, ftexcoord).r;
+ 
+    float distance_to_pixel = to_distance(depth_colour);
+    float x = clamp(abs(distance_to_pixel - model_distance) / model_distance, 0.0, 1.0);
+    x = max(1.0 - x, 0.0);
+    x = 1.0 - pow(x, 1.0/10.0);
+
     vec2 radius = vec2(size/img_size.x, size/img_size.y);
     
     vec4 blurred_colour = texture(colour_tex, ftexcoord);
     
     for( float d=0.0; d<pi_times_2; d+= pi_times_2/directions)
 		for(float i=1.0/quality; i<=1.0; i+=1.0/quality)
-			blurred_colour += texture( colour_tex, ftexcoord + vec2(cos(d),sin(d))*radius*i);	
+			blurred_colour += texture( colour_tex, ftexcoord + vec2(cos(d),sin(d))*radius*x*i);	
     
     // Output to screen
     blurred_colour /= quality * directions;// - 15.0;
 
-	vec4 unblurred_colour = texture(colour_tex, ftexcoord);
-
-	float depth_colour = texture(depth_tex, ftexcoord).r;
- 
-    float distance_to_pixel = to_distance(depth_colour);
-
-
-
-    float x = clamp(abs(distance_to_pixel - model_distance) / model_distance, 0.0, 1.0);
-
-     x = max(1 - x, 0);
-
-     x = 1.0 - pow(x, 1.0/10.0);
-
-
-
-    frag_colour.rgb = vec3(mix(unblurred_colour, blurred_colour, x));
+    frag_colour.rgb = blurred_colour.rgb;//vec3(mix(unblurred_colour, blurred_colour, x));
     frag_colour.a = 1.0;
 }
 
