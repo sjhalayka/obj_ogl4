5a57dbb26b1c63821693e7ec7e6c6dba791845e2
Shawn Halayka <sjhalayka@gmail.com>
2022-10-30 21:25:03 -0600

    Add files via upload

--- ortho_reflectance.fs.glsl
+++ ortho_reflectance.fs.glsl
@@ -3,7 +3,7 @@
 uniform sampler2D regular_tex; // texture uniform
 uniform sampler2D upside_down_tex; // texture uniform
 uniform sampler2D reflectance; // texture uniform
-
+uniform sampler2D upside_down_white_mask;
 
 in vec2 ftexcoord;
 
@@ -11,7 +11,10 @@ layout(location = 0) out vec4 frag_colour;
 
 void main()
 {
-    frag_colour = mix(texture(regular_tex, ftexcoord), texture(upside_down_tex, ftexcoord), texture(reflectance, ftexcoord));
+    frag_colour = mix(texture(regular_tex, ftexcoord), 
+                    texture(upside_down_tex, ftexcoord),
+                    texture(reflectance, ftexcoord)*texture(upside_down_white_mask, ftexcoord));
+
     frag_colour.a = 1.0;
 }
 
