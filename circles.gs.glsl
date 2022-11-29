776d0222f09c134e40956c706c3b41e6bcac7baa
Shawn Halayka <sjhalayka@gmail.com>
2022-11-28 22:55:09 -0600

    Add files via upload

--- circles.gs.glsl
+++ circles.gs.glsl
@@ -1,6 +1,6 @@
 #version 430
 layout (points) in;                              
-layout (triangle_strip, max_vertices = 48) out;  
+layout (triangle_strip, max_vertices = 34) out; // 16 steps
 
 
 uniform int img_width;
@@ -10,41 +10,27 @@ uniform vec3 camera_pos; // assumes that the lookat is effectively vec3(0, 0, 0)
 
 uniform float line_thickness;
 
-vec2  u_viewportSize = vec2(img_width, img_height);
-float u_thickness = cam_factor * line_thickness;
-
-
+const vec2  u_viewportSize = vec2(img_width, img_height);
+const float u_thickness = cam_factor * line_thickness;
+const float steps = 16;
+const float pi = 4.0*atan(1.0);
 
 void main()
 {
-    vec4 position = gl_in[0].gl_Position;
+    const vec4 position = gl_in[0].gl_Position;
+    const vec2 offset = vec2(u_thickness / u_viewportSize);
 
-    vec2 offset = vec2(u_thickness / u_viewportSize);
-
-    float r_x = offset.x;
-    float r_y = offset.y;
-
-    const float pi = 4.0*atan(1.0);
-    const float pi_times_2 = pi*2;
-    
-    float directions = 16;
-
-    for( float phi = -pi/2; phi < pi/2; phi += pi/directions)
+    for( float phi = -pi/2; phi <= pi/2; phi += pi/steps)
     {
-        float x1 = r_x * sin(phi);
-        float y1 = r_y * cos(phi);
+        const float x1 = offset.x * sin(phi);
+        const float y1 = offset.y * cos(phi);
 
-        gl_Position = position + vec4(x1 * position.w, y1 * position.w, 0.0, 0.0);
+        gl_Position = position + vec4(x1 * position.w,  y1 * position.w, 0.0, 0.0);
         EmitVertex();
 
         gl_Position = position + vec4(x1 * position.w, -y1 * position.w, 0.0, 0.0);
         EmitVertex();
     }
-
-    float x_last = r_x * sin(pi/2);
-    float y_last = r_y * cos(pi/2);
-    gl_Position = position + vec4(x_last * position.w, y_last * position.w, 0.0, 0.0);
-    EmitVertex();
 
     EndPrimitive();
 
